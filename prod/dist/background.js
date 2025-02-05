e1cad6d0b4f532af7ee9e2d969a80666ba08249f
spin311 <79504899+spin311@users.noreply.github.com>
2025-02-05 13:29:08 +0100

    prod build

--- prod/dist/background.js
+++ prod/dist/background.js
@@ -8,7 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var Reason = chrome.offscreen.Reason;
 var ContextType = chrome.runtime.ContextType;
 const AUDIO_ACTIVE = "audioActive";
@@ -17,22 +16,35 @@ const OPEN_PROLIFIC = "openProlific";
 const AUDIO = "audio";
 const VOLUME = "volume";
 const COUNTER = "counter";
+const FOCUS_PROLIFIC = "focusProlific";
+const NU_PLACES = "nuPlaces";
+const REWARD = "reward";
+const REWARD_PER_HOUR = "rewardPerHour";
+const ACTIVE_TAB = "activeTab";
 const ICON_URL = 'imgs/logo.png';
 const TITLE = 'Prolific Automatic Studies';
-const MESSAGE = 'A new study has been posted on Prolific!';
-let creating; // A global promise to avoid concurrency issues
+const MESSAGE = 'A new study is available on Prolific!';
+let creating = null; // A global promise to avoid concurrency issues
 let volume;
 let audio;
-let shouldSendNotification;
-let shouldPlayAudio;
 chrome.runtime.onMessage.addListener(handleMessages);
 chrome.notifications.onClicked.addListener(function (notificationId) {
-    chrome.tabs.create({ url: "https://app.prolific.com/", active: true });
+    if (!!notificationId) {
+        chrome.tabs.create({ url: `https://app.prolific.com/studies/${notificationId}`, active: true });
+    }
+    else {
+        chrome.tabs.create({ url: "https://app.prolific.com/", active: true });
+    }
     chrome.notifications.clear(notificationId);
 });
 chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
     if (buttonIndex === 0) {
-        chrome.tabs.create({ url: "https://app.prolific.com/", active: true });
+        if (!!notificationId) {
+            chrome.tabs.create({ url: `https://app.prolific.com/studies/${notificationId}`, active: true });
+        }
+        else {
+            chrome.tabs.create({ url: "https://app.prolific.com/", active: true });
+        }
     }
     chrome.notifications.clear(notificationId);
 });
@@ -70,6 +82,54 @@ function handleMessages(message) {
             case 'clear-badge':
                 yield chrome.action.setBadgeText({ text: '' });
                 break;
+            case 'new-studies':
+                let studies = message.data;
+                if (!studies)
+                    break;
+                const shouldShowNotification = yield getValueFromStorage(SHOW_NOTIFICATION, true);
+                const shouldPlayAudio = yield getValueFromStorage(AUDIO_ACTIVE, true);
+                const shouldFocusProlific = yield getValueFromStorage(FOCUS_PROLIFIC, false);
+                const numPlaces = yield getValueFromStorage(NU_PLACES, 0);
+                const reward = yield getValueFromStorage(REWARD, 0);
+                const rewardPerHour = yield getValueFromStorage(REWARD_PER_HOUR, 0);
+                if (numPlaces > 0 || reward > 0 || rewardPerHour > 0) {
+                    studies = studies.filter((study) => {
+                        if (numPlaces && study.places && study.places <= numPlaces) {
+                            return false;
+                        }
+                        if (reward && study.reward && getFloatValueFromMoneyString(study.reward) <= reward) {
+                            return false;
+                        }
+                        return !(rewardPerHour && study.rewardPerHour && getFloatValueFromMoneyString(study.rewardPerHour) <= rewardPerHour);
+                    });
+                }
+                if (studies.length === 0)
+                    break;
+                if (shouldPlayAudio) {
+                    audio = yield getValueFromStorage(AUDIO, 'alert1.mp3');
+                    volume = (yield getValueFromStorage(VOLUME, 100)) / 100;
+                    yield playAudio(audio, volume);
+                }
+                if (shouldFocusProlific) {
+                    const tabs = yield chrome.tabs.query({ url: "*://app.prolific.com/*" });
+                    if (tabs.length > 0) {
+                        yield chrome.tabs.update(tabs[0].id, { active: true });
+                    }
+                    else {
+                        yield chrome.tabs.create({ url: "https://app.prolific.com/", active: true });
+                    }
+                }
+                if (shouldShowNotification) {
+                    studies
+                        .sort((a, b) => getFloatValueFromMoneyString(b.reward || "0") - getFloatValueFromMoneyString(a.reward || "0"))
+                        .forEach((study) => {
+                        setTimeout(() => {
+                            sendNotification(study);
+                        }, 3000);
+                    });
+                }
+                yield updateCounterAndBadge(studies.length);
+                break;
         }
     });
 }
@@ -94,112 +154,40 @@ function playAudio() {
         });
     });
 }
-function checkNewStudies() {
-    return __awaiter(this, void 0, void 0, function* () {
-        var _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
-        const targetNode = document.querySelector('ul.list'); // Prolific study list
-        if (!targetNode) {
-            console.log('Target node not found.');
-            return;
-        }
-        // Fetch saved settings and storage values
-        const savedStudiesIds = yield getValueFromStorage('studies', []);
-        shouldSendNotification = yield getValueFromStorage(SHOW_NOTIFICATION, true);
-        shouldPlayAudio = yield getValueFromStorage(AUDIO_ACTIVE, true);
-        const audio = shouldPlayAudio ? yield getValueFromStorage(AUDIO, 'alert1.mp3') : null;
-        const volume = shouldPlayAudio ? (yield getValueFromStorage(VOLUME, 100)) / 100 : null;
-        const studyList = document.querySelectorAll('ul.list li');
-        let newStudiesCount = 0;
-        const currentStudies = [];
-        // Iterate over all study items
-        for (const study of studyList) {
-            const id = (_c = study.getAttribute('data-testid')) === null || _c === void 0 ? void 0 : _c.split('-')[1];
-            const title = (_d = study.querySelector('[data-testid="title"]')) === null || _d === void 0 ? void 0 : _d.textContent;
-            const researcher = (_f = (_e = study.querySelector('[data-testid="host"]')) === null || _e === void 0 ? void 0 : _e.textContent) === null || _f === void 0 ? void 0 : _f.split(' ')[1];
-            const places = parseInt(((_h = (_g = study.querySelector('[data-testid="study-tag-places"]')) === null || _g === void 0 ? void 0 : _g.textContent) === null || _h === void 0 ? void 0 : _h.split(' ')[0]) || '0');
-            const reward = parseFloat(((_k = (_j = study.querySelector('[data-testid="study-tag-reward"]')) === null || _j === void 0 ? void 0 : _j.textContent) === null || _k === void 0 ? void 0 : _k.replace('$', '')) || '0');
-            const rewardPerHour = parseFloat(((_m = (_l = study.querySelector('[data-testid="study-tag-reward-per-hour"]')) === null || _l === void 0 ? void 0 : _l.textContent) === null || _m === void 0 ? void 0 : _m.replace('$', '').replace('/hr', '')) || '0');
-            const time = getTimeFromString(study);
-            if (id) {
-                const currentStudy = {
-                    id,
-                    researcher,
-                    title,
-                    reward,
-                    places,
-                    rewardPerHour,
-                    time,
-                    limitedCapacity: false,
-                };
-                // Check if the study is new
-                if (!savedStudiesIds.includes(id)) {
-                    newStudiesCount++;
-                    if (shouldSendNotification) {
-                        sendNotification(currentStudy);
-                    }
-                }
-                currentStudies.push(currentStudy);
-            }
-            else {
-                console.log('Study ID not found for an item.');
-            }
-        }
-        // Handle updates for new studies
-        if (newStudiesCount > 0) {
-            if (shouldPlayAudio && audio) {
-                yield playAudio(audio, volume);
-            }
-            yield updateCounterAndBadge(newStudiesCount);
-            yield chrome.storage.sync.set({ "studies": currentStudies.map(study => study.id) });
-        }
-    });
-}
-chrome.tabs.onUpdated.addListener((_a, _b, tab) => __awaiter(void 0, void 0, void 0, function* () {
-    if (tab.id && tab.url && tab.url.includes('https://app.prolific.com') && tab.status === 'complete') {
-        yield chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: checkNewStudies,
-        });
-    }
-}));
-function getTimeFromString(study) {
-    var _c;
-    const timeText = (_c = study.querySelector('[data-testid="study-tag-completion-time"]')) === null || _c === void 0 ? void 0 : _c.textContent;
-    let time = 0;
-    if (timeText) {
-        const timeParts = timeText.match(/(\d+)\s*h\s*(\d+)?\s*mins?/);
-        if (timeParts) {
-            const hours = parseInt(timeParts[1]) || 0;
-            const minutes = parseInt(timeParts[2]) || 0;
-            time = hours * 60 + minutes;
-        }
-        else {
-            const minutesOnly = timeText.match(/(\d+)\s*mins?/);
-            if (minutesOnly) {
-                time = parseInt(minutesOnly[1]);
-            }
-        }
-    }
-    return time;
-}
 function setInitialValues() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield Promise.all([
-            chrome.storage.sync.set({ [AUDIO_ACTIVE]: true }),
-            chrome.storage.sync.set({ [AUDIO]: "alert1.mp3" }),
-            chrome.storage.sync.set({ [SHOW_NOTIFICATION]: true }),
-            chrome.storage.sync.set({ [VOLUME]: 100 }),
-        ]);
+        yield chrome.storage.sync.set({
+            [AUDIO_ACTIVE]: true,
+            [AUDIO]: "alert1.mp3",
+            [SHOW_NOTIFICATION]: true,
+            [VOLUME]: 100,
+            [ACTIVE_TAB]: "settings",
+        });
     });
 }
 function sendNotification(study = null) {
-    chrome.notifications.create({
+    let title = TITLE;
+    let message = MESSAGE;
+    let id = "";
+    if (study) {
+        if (study.id) {
+            id = study.id;
+        }
+        if (study.title && study.researcher) {
+            title = `${study.title}\nBy ${study.researcher}`;
+        }
+        if (study.reward && study.time && study.rewardPerHour && study.places) {
+            message += `\nReward: ${study.reward}\nReward per hour: ${study.rewardPerHour}\nTime: ${study.time} | Places: ${study.places}`;
+        }
+    }
+    const options = {
         type: 'basic',
         iconUrl: chrome.runtime.getURL(ICON_URL),
-        title: TITLE,
-        message: MESSAGE,
-        buttons: [{ title: 'Open Prolific' }, { title: 'Dismiss' }],
-    });
+        title: title,
+        message: message,
+        buttons: [{ title: 'Open Study' }, { title: 'Dismiss' }],
+    };
+    chrome.notifications.create(id, options);
 }
 function updateBadge(counter) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -242,4 +230,16 @@ function setupOffscreenDocument(path) {
             creating = null;
         }
     });
+}
+function getFloatValueFromMoneyString(value) {
+    const firstWord = value.split(" ")[0];
+    if (firstWord.charAt(0) === '£') {
+        return parseFloat(firstWord.slice(1));
+    }
+    else if (firstWord.charAt(0) === '$') {
+        return parseFloat(firstWord.slice(1)) * 0.8;
+    }
+    else {
+        return 0;
+    }
 }
